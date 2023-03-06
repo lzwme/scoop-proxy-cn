45bf7e42dd81d18ffc25f2135290313f313ed177
renxia <lzwy0820@qq.com>
2023-03-06 10:26:59 +0800

    perf(sync): 增加输出 sync-sources.txt 文件记录来源统计

--- .github/sync.mjs
+++ .github/sync.mjs
@@ -1,15 +1,22 @@
 import { execSync } from 'node:child_process';
 import fs from 'node:fs';
 import path from 'node:path';
-import { semverCompare, color } from '@lzwme/fe-utils';
+import { fileURLToPath } from 'node:url';
+import { semverCompare, color, getLogger } from '@lzwme/fe-utils';
 import { safeJsonParse } from './utils.mjs';
 
 const isDebug = process.argv.slice(2).includes('--debug');
 const isCI = process.env.SYNC != null;
-const CONSTS = {
-  rootDir: process.cwd(),
-  tmpDir: path.resolve('tmp'),
+const logger = getLogger('SYNC', isDebug ? 'debug' : 'log');
+const rootDir = path.resolve(fileURLToPath(import.meta.url), '../..');
+const CONFIG = {
+  rootDir,
+  /** 是否忽略 manifest JSON 解析失败的应用 */
+  ignoreParseFailed: false,
+  tmpDir: path.resolve(rootDir, 'tmp'),
+  /** 同步仓库列表，按仓库质量排序 */
   repo: new Set([
+    // `duzyn/scoop-cn`, // 仅同步官方仓库，并修改为国内镜像地址
     `ScoopInstaller/PHP`,
     `ScoopInstaller/Main`,
     `ScoopInstaller/Extras`,
@@ -17,8 +24,10 @@ const CONSTS = {
     `ScoopInstaller/Versions`,
     `ScoopInstaller/Nirsoft`,
     `ScoopInstaller/Nonportable`,
-    `scoopcn/scoopcn`,
+    `scoopcn/scoopcn`, // 大多是国内应用程序
+    `rasa/scoops`,
     `ACooper81/scoop-apps`,
+    `kkzzhizhou/scoop-zapps`,
     `Calinou/scoop-games`,
     `cderv/r-bucket`,
     `chawyehsu/dorado`,
@@ -36,18 +45,14 @@ const CONSTS = {
     `TheRandomLabs/scoop-nonportable`,
     `TheRandomLabs/Scoop-Spotify`,
     `Paxxs/Cluttered-bucket`,
-    `rasa/scoops`,
     `zhoujin7/tomato`,
     `HCLonely/my-scoop-bucket`,
     `Weidows-projects/scoop-3rd`,
     `hermanjustnu/scoop-emulators`,
     `everyx/scoop-bucket`,
-    // `dodorz/scoop`,
     `borger/scoop-emulators`,
-    // `Qv2ray/mochi`,
     `ZvonimirSun/scoop-iszy`,
     `kiennq/scoop-misc`,
-    // `wangzq/scoop-bucket`,
     `wzv5/ScoopBucket`,
     `TheRandomLabs/Scoop-Python`,
     `naderi/scoop-bucket`,
@@ -56,60 +61,64 @@ const CONSTS = {
     `akirco/aki-apps`,
     `batkiz/backit`,
     `iquiw/scoop-bucket`,
-    // `NyaMisty/scoop_bucket_misty`,
     `ygguorun/scoop-bucket`,
-    // `TheLastZombie/scoop-bucket`,
     `seumsc/scoop-seu`,
-    // `Velgus/Scoop-Portapps`,
     `cc713/ownscoop`,
-    // `amorphobia/siku`,
-    // `jonz94/scoop-sarasa-nerd-fonts`,
-    `rivy/scoop-bucket`,
     `aoisummer/scoop-bucket`,
     `yuusakuri/scoop-bucket`,
     `hu3rror/scoop-muggle`,
-    // `starise/Scoop-Gaming`,
     `starise/Scoop-Confetti`,
-    // `Darkatse/Scoop-KanColle`,
-    `hulucc/bucket`,
+    // `dodorz/scoop`, // 0
+    // `Qv2ray/mochi`, // 0
+    // `wangzq/scoop-bucket`, // 0
+    // `NyaMisty/scoop_bucket_misty`, // 0
+    // `TheLastZombie/scoop-bucket`, // 0
+    // `Velgus/Scoop-Portapps`, // 0
+    // `amorphobia/siku`, // 0
+    // `jonz94/scoop-sarasa-nerd-fonts`, // 0
+    // `rivy/scoop-bucket`, // 0
+    // `starise/Scoop-Gaming`, // 0
+    // `Darkatse/Scoop-KanColle`, // 0
+    // `hulucc/bucket`, // 0
+    // `Deide/deide-bucket`, // 0
     `jingyu9575/scoop-jingyu9575`,
-    // `Deide/deide-bucket`,
-    `okibcn/ScoopMaster`,
-    `anderlli0053/DEV-tools`,
-    // `kkzzhizhou/scoop-apps`,
     // `tetradice/scoop-iyokan-jp`,
-    // `duzyn/scoop-cn`,
+    // `kkzzhizhou/scoop-apps`, // 抓其他仓库自动同步【会将 todo、deprecated 的都抓进来】
+    `anderlli0053/DEV-tools`, // low quality
+    `okibcn/ScoopMaster`, // 抓取其他仓库(https://rasa.github.io/scoop-directory/by-apps.html)自动同步(879+)
   ]),
   filter: /audacity_installer|\.gitkeep|__/,
+  sourcesStatFile: path.resolve(rootDir, `sync-sources.txt`),
+  lowPriorityFile: path.resolve(rootDir, 'bucket-low-priority.txt'),
+  ignoredFile: path.resolve(rootDir, 'bucket-ignored.txt'),
 };
 const destFilesCache = new Map();
-const bucketLowPriority = parseBucketTxtFile();
-const bucketIgnoreList = parseBucketTxtFile('bucket-ignored.txt');
+const lowPrioritySet = parseTxtFile(CONFIG.lowPriorityFile);
+const ignoredSet = parseTxtFile(CONFIG.ignoredFile);
 
-function parseBucketTxtFile(filename = 'bucket-low-priority.txt') {
-  const str = fs.readFileSync(path.resolve(CONSTS.rootDir, filename), 'utf8').trim();
+function parseTxtFile(filename) {
+  const str = fs.readFileSync(path.resolve(CONFIG.rootDir, filename), 'utf8').trim();
   const list = str
     .split('\n')
-    .slice(1)
-    .filter(d => d.includes(', '))
-    .map(d => path.resolve(CONSTS.tmpDir, d.replace('/', '-').replace(', ', '/')));
+    .filter(d => d && !d.startsWith('#'))
+    .map(d => path.resolve(CONFIG.tmpDir, d.replace('/', '-').replace(', ', '/')));
 
   return new Set(list);
 }
 
 async function checkout(repo, dirName) {
-  if (!fs.existsSync(CONSTS.tmpDir)) fs.mkdirSync(CONSTS.tmpDir);
+  if (!fs.existsSync(CONFIG.tmpDir)) fs.mkdirSync(CONFIG.tmpDir);
 
   try {
-    const dirpath = path.resolve(CONSTS.tmpDir, dirName);
+    const dirpath = path.resolve(CONFIG.tmpDir, dirName);
     if (fs.existsSync(dirpath)) {
-      execSync(`cd "${dirpath}" && git pull`, { cwd: CONSTS.tmpDir });
+      execSync(`cd "${dirpath}" && git pull`, { cwd: CONFIG.tmpDir });
     } else {
-      repo = isDebug ? `https://ghproxy.com/https://github.com/${repo}` : `https://github.com/${repo}.git`;
-      execSync(`git clone --depth 1 ${repo} ${dirName}`, { cwd: CONSTS.tmpDir });
+      repo = isDebug ? `https://ghproxy.com/github.com/${repo}` : `https://github.com/${repo}.git`;
+      execSync(`git clone --depth 1 ${repo} ${dirName}`, { cwd: CONFIG.tmpDir });
     }
   } catch (error) {
-    console.error(`checkout ${repo} failed!`, error.message);
+    logger.error(`checkout ${repo} failed!`, error.message);
   }
 }
 
@@ -117,45 +126,52 @@ async function syncDir(src, dest, repo = '') {
   let total = 0;
   const basename = path.basename(src);
 
-  if (!fs.existsSync(src) || CONSTS.filter.test(basename)) return total;
-  if (bucketIgnoreList.has(src)) return;
+  src = path.resolve(rootDir, src);
+  dest = path.resolve(rootDir, dest);
+
+  if (!fs.existsSync(src) || CONFIG.filter.test(basename)) return total;
+  if (ignoredSet.has(basename) || ignoredSet.has(src)) return;
 
   const stats = fs.statSync(src);
   const ext = path.extname(src);
 
   if (stats.isFile()) {
+    const srcRelative = src.slice(CONFIG.tmpDir.length + 1);
     const destLowerCase = String(dest).toLowerCase();
     let content = '';
     let contentJson;
 
     if (destFilesCache.has(destLowerCase)) {
-      if ('.json' !== ext || bucketLowPriority.has(src)) return total;
+      if ('.json' !== ext || lowPrioritySet.has(src) || lowPrioritySet.has(basename)) return total;
 
       dest = destFilesCache.get(destLowerCase).dest; // 使用旧路径
       try {
         // json 文件比较版本
         content = fs.readFileSync(src, 'utf8').trim();
         contentJson = safeJsonParse(content, src, true);
-        const destVersion = safeJsonParse(fs.readFileSync(dest, 'utf8'), dest).version || '';
-        if (semverCompare(contentJson.version || '', destVersion, false) < 1) return total;
-        if (isDebug) console.debug(`[sync]overwide: ${color.gray(contentJson.version)} -> ${color.green(destVersion)} ${color.cyan(src.slice(CONSTS.tmpDir.length + 1))}`);
+        const oldJson = safeJsonParse(fs.readFileSync(dest, 'utf8'), dest);
+        if (semverCompare(String(contentJson.version || ''), String(oldJson.version || ''), false) < 1) return total;
+        logger.debug(`[sync]overwide: ${color.gray(oldJson.version)} -> ${color.green(contentJson.version)} ${color.cyan(basename)} ${color.yellow(oldJson._from)}`);
       } catch (e) {
-        console.error('[error]try compare version failed!', src, dest, e.message);
+        logger.error('[error]try compare version failed!', src, dest, e.message);
         return total;
       }
     }
-    destFilesCache.set(destLowerCase, { dest, src: src.slice(CONSTS.tmpDir.length + 1), repo });
+    destFilesCache.set(destLowerCase, { dest, src: srcRelative, repo, fixed: false });
 
     if (['.json', '.ps1', '.sh'].includes(ext)) {
       if (!content) content = fs.readFileSync(src, 'utf8');
       const rawContent = content;
 
       if ('.json' === ext) {
-        if (!contentJson) contentJson = safeJsonParse(content, src);
+        if (!contentJson) contentJson = safeJsonParse(content, srcRelative);
         if (Object.keys(contentJson).length > 0) {
           contentJson._from = repo;
           content = JSON.stringify(contentJson, null, 2);
-        } else content = content.replaceAll('\r\n', '\n').trim();
+        } else {
+          if (CONFIG.ignoreParseFailed) return total;
+          content = content.replaceAll('\r\n', '\n').trim();
+        }
 
         // fix for https://github.com/lzwme/scoop-proxy-cn/issues/2
         content = content.replace(/\$bucketsdir\\\\[a-zA-Z\-]+\\\\/gim, '$bucketsdir\\\\$bucket\\\\');
@@ -198,7 +214,7 @@ async function syncDir(src, dest, repo = '') {
 async function gitCommit() {
   const changes = execSync('git status --short', { encoding: 'utf8' }).trim(); // --untracked-files=no
   if (changes.length > 5) {
-    console.log('Changes:\n', changes);
+    logger.info('Changes:\n', changes);
     const cmds = [
       `git config user.name "github-actions[bot]"`,
       `git config user.email "41898282+github-actions[bot]@users.noreply.github.com"`,
@@ -209,49 +225,70 @@ async function gitCommit() {
 
     for (const cmd of cmds) execSync(cmd, { encoding: 'utf8', maxBuffer: 1024 * 1024 * 100 });
   } else {
-    console.log('Not Updated');
+    logger.info('Not Updated');
   }
 }
+function outputSources() {
+  logger.debug('starting output for', CONFIG.sourcesStatFile);
+
+  const content = [...destFilesCache.values()]
+    .sort((a, b) => a.src > b.src)
+    .map(item => {
+      return `${item.src.replace(CONFIG.tmpDir), ''}, ${repo}, ${item.fixed ? 1 : 0}`;
+    })
+    .join('\n');
+  fs.writeFileSync(CONFIG.sourcesStatFile, content, 'utf8');
+}
+
 async function updateReadme() {
-  const list = [...CONSTS.repo].map(repo => `- [https://github.com/${repo}](${repo})`).join('\n');
-  const content = fs.readFileSync('README.md', 'utf8');
+  const rdFile = path.resolve(rootDir, 'README.md');
+  const list = [...CONFIG.repo].map(repo => `- [https://github.com/${repo}](${repo})`).join('\n');
+  const content = fs.readFileSync(rdFile, 'utf8');
   const updated = content.replace(/## Sync Buckets From[\s\S]+##/g, `## Sync Buckets From\n\n${list}\n\n##`);
-  if (updated !== content) fs.writeFileSync(updated, content, 'utf8');
+  if (updated !== content) fs.writeFileSync(rdFile, updated, 'utf8');
+  else logger.debug('[UPDATE-READE] No Chagned');
 }
 
 function updateInstallps1() {
+  logger.debug('starting update install.ps1');
   const installUrl = `${isDebug ? 'https://ghproxy.com/' : ''}https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1`;
   execSync(`curl ${installUrl} > install.ps1`);
   syncDir('install.ps1', 'install.ps1');
 }
 
 async function sync() {
+  const stats = {
+    sync: { bucket: 0, scripts: 0 },
+    repo: {},
+  };
+
   updateInstallps1();
   if (isCI) {
-    ['bucket', 'scripts', 'tmp'].forEach(d => fs.existsSync(d) && fs.rmSync(d, { recursive: true, force: true }));
+    [...Object.keys(stats.sync), 'tmp'].forEach(d => fs.existsSync(d) && fs.rmSync(d, { recursive: true, force: true }));
   }
 
-  const stats = {};
-
-  for (const repo of CONSTS.repo) {
+  for (const repo of CONFIG.repo) {
     const repoDirName = repo.replaceAll('/', '-');
-    console.log(`sync for ${color.greenBright(repo)}`);
+    logger.info(`sync for ${color.greenBright(repo)}`);
     await checkout(repo, repoDirName);
-    for (const fname of ['bucket', 'scripts']) {
-      const count = await syncDir(path.resolve(CONSTS.tmpDir, repoDirName, fname), fname, repo);
-      if (!count && fname === 'bucket') console.warn(`[warn][${fname}][synced nothing]`, repo);
-      else console.log(` - [synced][${color.green(fname)}]`, count);
-      stats[fname] = (stats[fname] || 0) + count;
+    stats.repo[repo] = {};
+    for (const fname of Object.keys(stats.sync)) {
+      stats.repo[repo][fname] = await syncDir(path.resolve(CONFIG.tmpDir, repoDirName, fname), fname, repo);
+      if (!stats.sync[fname] && fname === 'bucket') logger.warn(`[warn][${fname}][synced nothing]`, color.yellowBright(repo));
+      else logger.info(` - [synced][${color.green(fname)}]`, stats.sync[fname]);
+      stats.sync[fname] += stats.sync[fname];
     }
   }
 
   if (isCI) {
-    fs.rmSync(CONSTS.tmpDir, { recursive: true, force: true });
-    gitCommit();
+    fs.rmSync(CONFIG.tmpDir, { recursive: true, force: true });
+    outputSources();
     updateReadme();
+    gitCommit();
   }
 
-  console.log('Done!', `Total: ${destFilesCache.size}`, stats);
+  const fixedCount = [...destFilesCache.values()].filter(d => d.fixed).length;
+  logger.info('Done!', `Total: ${destFilesCache.size}, Fixed: ${fixedCount}`, stats);
 }
 
 sync();
